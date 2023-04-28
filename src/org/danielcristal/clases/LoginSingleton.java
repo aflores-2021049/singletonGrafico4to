a7bec2b55f51d12d958e84065c567804b33223bb
Andres Flores <aflores-2021049@kinal.edu.gt>
2023-04-28 22:33:00 +0200

    primer commit

---
+++ src/org/danielcristal/clases/LoginSingleton.java
@@ -0,0 +1,24 @@
+/*
+creac el constructor de la clase public privada
+declarar una variable privada y estatica del formulario que se requiere utilizar(jFrame)
+agrergar libreria
+crearemos un metodo para la instancia del objeto
+agregar el paquete de login
+*/
+package org.danielcristal.clases;
+
+public class LoginSingleton {
+
+    private LoginSingleton() {
+        private static jFrame login;
+        
+        public static jFrame getInstance();
+        if (log == null)
+        instance login;
+        
+        
+        
+
+    }
+    
+}
